52102b7e5865906a97aa2b111b09ab223e1fff5b
wonko <crangos@web.de>
2012-01-01 18:08:07 -0500

    renamed plot_stim.m to better reflect it plotting the whole protocol

---
+++ plot_protocol.m
@@ -0,0 +1,21 @@
+function elapsed = plot_protocol(X, Fs)
+
+plottime = tic;
+
+    % plot
+    figure( 1 ); 
+    clf
+    ah1 = axes( 'position', [ 0.13         0.51      0.73119      0.34116 ] );
+
+    plot( [1:size(X,1)]/Fs, X )
+    ylim( [ 0 5 ] )
+    xlim( [ 1 size(X, 1) ] / Fs )
+    %subplot( 2, 1, 2 )
+    ah2 = axes( 'position', [ 0.13         0.11      0.78519      0.34116 ] )
+    imagesc( [1:size(X,1)]/Fs, 1:size(X,2), X', [ 0 5 ] ), axis xy
+    xlabel( 'Time (sec)' )
+    colorbar
+
+elapsed = toc(plottime);
+
+end
