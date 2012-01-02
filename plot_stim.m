02217b30291941a4e8065c3ab1059fab097630b7
wonko <crangos@web.de>
2012-01-01 19:00:32 -0500

    Added load_template.m

--- plot_stim.m
+++ plot_stim.m
@@ -1,4 +1,4 @@
-function elapsed = plot_protocol(X, Fs)
+function elapsed = plot_stim(X, Fs)
 
 plottime = tic;
 
@@ -11,7 +11,7 @@ plottime = tic;
     ylim( [ 0 5 ] )
     xlim( [ 1 size(X, 1) ] / Fs )
     %subplot( 2, 1, 2 )
-    ah2 = axes( 'position', [ 0.13         0.11      0.78519      0.34116 ] )
+    ah2 = axes( 'position', [ 0.13         0.11      0.78519      0.34116 ] );
     imagesc( [1:size(X,1)]/Fs, 1:size(X,2), X', [ 0 5 ] ), axis xy
     xlabel( 'Time (sec)' )
     colorbar
