ee19705e9284d5e09a92f2484326ecfb3e6a521f
wonko <crangos@web.de>
2012-01-10 11:29:59 -0500

    Overwriting loaded fields with deal_fields for quick adaptation of templates/protocols _ missed files

---
+++ deal_fields.m
@@ -0,0 +1,32 @@
+% For the given [desc struct], replaces the values of the [fields] with
+% new [values] using deal()
+
+function desc = deal_fields(desc, fields, values)
+
+if ischar(fields)
+    nfields = 1;
+    fields = {fields}
+else
+    nfields = numel(fields);
+end
+
+if numel(values) ~= numel(desc)
+    disp('ooops');
+end
+
+if ~iscell(values)
+    values = {values};
+end
+
+keyboard
+% not sufficient! Dealing values to structs is tricky business
+for v = 1:nfields
+    nchans = numel(desc);
+    m = ceil(nchans/numel(values{v}));
+    values{v} = repmat(values{v}, 1, m);
+
+%     keyboard
+    for channel = 1:nchans
+            desc(channel).(fields{v}) = values{v}(channel);
+    end
+end
