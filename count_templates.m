4b2c8bc1ce1cf0407754a4d65a71fcc58de2ecf8
wonko <crangos@web.de>
2012-01-12 02:48:44 -0500

    "Minor cleanup"

--- count_templates.m
+++ count_templates.m
@@ -6,16 +6,20 @@ ntemplates = 0;
     names = fieldnames(desc);
     if any(strcmp('template', names))
         disp('--> Given struct has template field!');
-        
-        for chan = 1:size(desc, 2)
-            if size(desc(chan).template, 2) > 1 && ~ischar(desc(chan).template)
-                if DEBUG disp(['    Multiple entries! Channel: ', num2str(chan)]); end
-                for stim = 1:size(desc(chan).template, 2)
+        for chan = 1:numel(desc)
+            if numel(desc(chan).template) > 1 && ~ischar(desc(chan).template)
+                if DEBUG disp(['    Multiple entries? Channel: ', num2str(chan)]); end
+
+                % only add entries with non-empty template field instead of
+                % number elements in template cell!
+                for stim = 1:numel(desc(chan).template)
                     ntemplates = ntemplates + ~isempty(desc(chan).template{stim});
                 end
-            else 
-                if DEBUG disp(['    Single entry! Channel: ', num2str(chan)]); end
-                ntemplates = ntemplates + ~isempty(desc(chan).template);
+            else
+                empty = isempty(desc(chan).template);
+                ntemplates = ntemplates + empty;
+                if DEBUG && empty disp(['    Sinlge empty entry! Channel: ', num2str(chan)]); end
+                if DEBUG && ~empty disp(['    Single entry! Channel: ', num2str(chan)]); end
             end
         end
     end
